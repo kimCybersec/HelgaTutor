ae409039d36d7c61793b7ef01472491f713a08f0
Rai Kim <kim@host.client.com>
2025-06-18 23:41:23 +0300

    .

--- api/app.py
+++ api/app.py
@@ -5,8 +5,8 @@ from api.routes.chat import chat_bp
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
-    "https://mentalhealthbuddy-1.onrender.com",
-    "http://mentalhealthbuddy-1.onrender.com"
+    "https://helgatutor.onrender.com",
+    "https://helgatutorapi.onrender.com"
 ]}})
 
 app.register_blueprint(chat_bp, url_prefix="/api/chat")
